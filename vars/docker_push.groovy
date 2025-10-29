857a83783ebeb52307ee9c6692c2d60182d314dc
Sourav Patel <souravpatel65@gmail.com>
2025-10-29 12:44:11 +0530

    Add docker_push function for Docker image handling

---
+++ vars/docker_push.groovy
@@ -0,0 +1,18 @@
+def call(String credId, String imageName){
+
+
+withCredentials([usernamePassword(
+                    credentialsId:"${credId}",
+                    passwordVariable: "dockerHubPass",
+                    usernameVariable: "dockerHubUser"
+                
+                )]){
+                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+                sh "docker image tag ${imageName} ${env.dockerHubUser}/${imageName}"
+                sh "docker push ${env.dockerHubUser}/${imageName}:latest"
+                
+                }
+
+
+  
+}
